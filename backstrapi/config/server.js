5a6e728e045aed7c1acbfdf1f74cc55c1dc9719a
Youssef Masry <youssefmasry888@gmail.com>
2026-08-19 07:02:11 +0300

    Set Strapi public URL

--- backstrapi/config/server.js
+++ backstrapi/config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
+  url: env('PUBLIC_URL', 'https://homey-strapi.onrender.com'),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
