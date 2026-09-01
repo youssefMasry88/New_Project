7693dbf1d97757e5b3136980bf5270aca3307b4a
Youssef Masry <youssefmasry888@gmail.com>
2026-09-01 06:40:51 +0300

    fix cloudinary CSP

--- backstrapi/config/middlewares.js
+++ backstrapi/config/middlewares.js
@@ -1,7 +1,37 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+            'https://strapi-ai-staging.s3.us-east-1.amazonaws.com',
+            'https://market-assets.strapi.io',
+          ],
+
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+            'https://strapi-ai-staging.s3.us-east-1.amazonaws.com',
+            'https://market-assets.strapi.io',
+          ],
+        },
+      },
+    },
+  },
+
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
